f64432a42c6e55080bbb5db70100f111ebd8b81c
AlexShy1337 <AlexShy1337@users.github.com>
2020-03-29 23:56:43 +0300

    created WishController

---
+++ Wish Box/Controllers/WishController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wish_Box.Models;
+
+namespace Wish_Box.Controllers
+{
+    public class WishController : Controller
+    {
+        private AppDbContext db;
+
+        public WishController(AppDbContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Wishes.ToListAsync());
+        }
+
+        //how to include userId in wish, the author of the wish
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Wish wish)
+        {
+            db.Wishes.Add(wish);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                Wish wish = await db.Wishes.FirstOrDefaultAsync(p => p.Id == id);
+                if (wish != null)
+                    return View(wish);
+            }
+            return NotFound();
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Wish wish = await db.Wishes.FirstOrDefaultAsync(p => p.Id == id);
+                if (wish != null)
+                    return View(wish);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Wish wish)
+        {
+                db.Wishes.Update(wish);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Wish wish = await db.Wishes.FirstOrDefaultAsync(p => p.Id == id);
+                if (wish != null)
+                    return View(wish);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                Wish wish = new Wish { Id = id.Value };
+                db.Entry(wish).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
--- Wish Box/Models/Wish.cs
+++ Wish Box/Models/Wish.cs
@@ -8,10 +8,12 @@ namespace Wish_Box.Models
     public class Wish
     {
         public int Id { get; set; }
+        //I'm unable to change DB in any way, so someone else should do it
+        //add this ↓ please
+        //public string Name { get; set; }
         public string Description { get; set; }
         public bool IsTaken { get; set; }
-        
         public int UserId {get;set;}
-        public User User { get; set; }
+        //public User User { get; set; }
     }
 }
