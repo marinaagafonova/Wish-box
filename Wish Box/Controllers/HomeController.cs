07ee32d06e0d89146a1a6fd8e40b829a26de8d3e
marinaagafonova <agafonova@cs.vsu.ru>
2020-04-01 22:21:01 +0300

    user search: filter

--- Wish Box/Controllers/HomeController.cs
+++ Wish Box/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
 using Wish_Box.Models;
 using PagedList.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Wish_Box.Controllers
 {
@@ -47,14 +49,67 @@ namespace Wish_Box.Controllers
         }
 
         [HttpGet] 
-        [Route("Search")]
-        public async Task<IActionResult> Search(int page = 1, int pageSize = 3)
+        public async Task<IActionResult> Search(UserListViewModel model = null)
         {
             var keyword = Request.Query["keyword"].ToString();
             var users = db.Users.Where(u => u.Login.Contains(keyword));
-            PagedList<User> model = new PagedList<User>(users, page, pageSize);
+            List<string> countries = GetCountries();
+            List<string> cities = GetCities();
+            if(model.Users == null)
+            {
+                model = new UserListViewModel
+                {
+                    Users = users.ToList(),
+                    Countries = new SelectList(countries),
+                    Cities = new SelectList(cities),
+                    Name = keyword
+                };
+            }
             ViewBag.keyword = keyword;
             return View(model);
+        }
+
+        private List<string> GetCities()
+        {
+            List<string> cities = db.Users.Select(p => p.City).ToList();
+            cities.Insert(0, "Все");
+            return cities;
+        }
+        private List<string> GetCountries()
+        {
+            List<string> countries = db.Users.Select(p => p.Country).ToList();
+            countries.Insert(0, "Все");
+            return countries;
+        }
+
+        public async Task<IActionResult> Filter(string country, string city, string name)
+        {
+            IQueryable<User> users = db.Users;
+            if (country != null && country != "" && country != "Все")
+            {
+                users = users.Where(p => p.Country == country);
+            }
+            if (city != null && city != "" && city != "Все")
+            {
+                users = users.Where(p => p.City == city);
+            }
+            if (!String.IsNullOrEmpty(name))
+            {
+                users = users.Where(p => p.Login.Contains(name));
+            }
+
+            List<string> countries = GetCountries();
+
+            List<string> cities = GetCities();
+
+            UserListViewModel viewModel = new UserListViewModel
+            {
+                Users = users.ToList(),
+                Countries = new SelectList(countries),
+                Cities = new SelectList(cities),
+                Name = name
+            };
+            return View("Search", viewModel);
         }
     }
 }
