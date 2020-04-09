33a16d10e4783f52363d4619e203c77964908f0e
KprvoAttano <34605858+KprvoAttano@users.noreply.github.com>
2020-04-09 19:02:12 +0300

    добавлена страница "мои пользователи"

---
+++ Wish Box/Controllers/FollowingsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wish_Box.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wish_Box.Controllers
+{
+    public class FollowingsController : Controller
+    {
+        AppDbContext db;
+
+        public FollowingsController(AppDbContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult Show()
+        {
+            var currentUser = db.Users.FirstOrDefault(x => x.Login == User.Identity.Name);
+
+            ViewBag.CurrentUser = currentUser;
+            ViewBag.AllUsers = db.Users.ToList();
+            return View(db.Followings.ToList());
+        }
+    }
+}
