5c9a089b2a88e0ffb5dd0e893fa4714202f7010f
KprvoAttano <34605858+KprvoAttano@users.noreply.github.com>
2020-04-09 21:48:01 +0300

    fix sort

--- Wish Box/Controllers/FollowingsController.cs
+++ Wish Box/Controllers/FollowingsController.cs
@@ -21,9 +21,19 @@ namespace Wish_Box.Controllers
         {
             var currentUser = db.Users.FirstOrDefault(x => x.Login == User.Identity.Name);
 
-            ViewBag.CurrentUser = currentUser;
-            ViewBag.AllUsers = db.Users.ToList();
-            return View(db.Followings.ToList());
+            List<User> followedUsersList = new List<User>();
+
+            foreach (var followed in db.Followings)
+            {
+                if (followed.UserFId == currentUser.Id)
+                {
+                    followedUsersList.Add(db.Users.FirstOrDefault(x => x.Id == followed.UserIsFId));
+                }
+            }
+
+            ViewBag.followingUsers = followedUsersList;
+
+            return View();
         }
     }
 }
