9f824ece3b9d8b03c68e4a56e26a687fd2e11db5
KprvoAttano <34605858+KprvoAttano@users.noreply.github.com>
2020-06-05 14:46:19 +0300

    add repos (user + following)

---
+++ Wish Box/Repositories/FollowingsRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wish_Box.Models;
+
+namespace Wish_Box.Repositories
+{
+    public class FollowingsRepository : IRepository<Following>
+    {
+        private AppDbContext db;
+
+        public FollowingsRepository(AppDbContext context)
+        {
+            db = context;
+        }
+        public void Create(Following item)
+        {
+            db.Followings.Add(item);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Following item = db.Followings.Find(id);
+            db.Followings.Remove(item);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<Following> Find(Func<Following, bool> predicate)
+        {
+            return db.Followings.Where(predicate).ToList();
+        }
+
+        public Following Get(int id)
+        {
+            Following item = db.Followings.Find(id);
+            return item;
+        }
+
+        public IEnumerable<Following> GetAll()
+        {
+            var list = db.Followings.ToList();
+
+            return list ?? new List<Following>();
+        }
+
+        public void Update(Following item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Wish Box/Repositories/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wish_Box.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Wish_Box.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        AppDbContext db;
+        public UserRepository(AppDbContext context)
+        {
+            db = context;
+        }
+        public void Create(User item)
+        {
+            db.Users.Add(item);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            User item = db.Users.Find(id);
+            db.Users.Remove(item);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<User> Find(Func<User, bool> predicate)
+        {
+            return db.Users.Where(predicate).ToList();
+        }
+
+        public User Get(int id)
+        {
+            User item = db.Users.Find(id);
+            return item;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            var list = db.Users.ToList();
+
+            return list ?? new List<User>();
+        }
+
+        public void Update(User item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
